b022c5ec44459b052e0ac6f6fbb83630df5c2197
Shawn Dossie <sdossie@gmail.com>
2009-08-21 12:09:24 -0400

    added 'sources' scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :sources
+
   map.resources :courses
 
   # The priority is based upon order of creation: first created -> highest priority.
